df8882e37ccffb36f1fb033874141e0f9ce3db74
ghosind <ghosind@gmail.com>
2024-07-04 21:46:22 +0800

    chore: move test cases to ts.

--- test/test_jtorch.spec.ts
+++ test/test_jtorch.spec.ts
@@ -1,15 +1,21 @@
-const { Tensor, isTensor } = require("../dist/jtorch");
-const SegHandler = require('segfault-handler');
-SegHandler.registerHandler('crash.log')
+import { Tensor, isTensor, arange } from "../dist/jtorch";
+import {registerHandler} from 'segfault-handler';
 
-const tensor = new Tensor();
+registerHandler('crash.log')
+
+let tensor = new Tensor();
 console.log(tensor.toString());
 const t = tensor.t();
 console.log(t.toString());
 const tt = t.t();
 console.log(tt.toString());
 
+// @ts-ignore
 console.log(isTensor(1), 1 instanceof Tensor);
 console.log(isTensor(tensor), tensor instanceof Tensor);
 
 console.log(new Tensor(tensor).toString())
+
+tensor = arange(5);
+console.log(tensor.toString());
+console.log(tensor.equal(arange(5)));
