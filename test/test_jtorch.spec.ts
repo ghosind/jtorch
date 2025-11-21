927a4573e3e57f250cca8f41b3f130d84f743c2d
Chen Su <ghosind@gmail.com>
2025-11-21 18:38:51 +0800

    test: update test cases
    Signed-off-by: Chen Su <ghosind@gmail.com>

--- test/test_jtorch.spec.ts
+++ test/test_jtorch.spec.ts
@@ -1,21 +1,42 @@
-import { Tensor, isTensor, arange } from "../dist/jtorch";
-import {registerHandler} from 'segfault-handler';
+/// <reference types="mocha" />
+import { expect } from 'chai';
+import { Tensor, isTensor, arange, isFloatingPoint, isNonzero } from "../dist/jtorch";
 
-registerHandler('crash.log')
+describe('jtorch core', () => {
+	it('constructors and type helpers', () => {
+		const empty = new Tensor();
+		expect(isTensor(1)).to.be.false;
+		expect(isTensor(empty)).to.be.true;
 
-let tensor = new Tensor();
-console.log(tensor.toString());
-const t = tensor.t();
-console.log(t.toString());
-const tt = t.t();
-console.log(tt.toString());
+		const copy = new Tensor(empty);
+		expect(isTensor(copy)).to.be.true;
+		expect(copy.toString()).to.equal(empty.toString());
+	});
 
-// @ts-ignore
-console.log(isTensor(1), 1 instanceof Tensor);
-console.log(isTensor(tensor), tensor instanceof Tensor);
+	it('arange, size, transpose and equal', () => {
+		const a1 = arange(5);
+		const a2 = arange(0, 5);
+		expect(isTensor(a1)).to.be.true;
+		expect(a1.equal(a2)).to.be.true;
+		expect(a1.size(0)).to.equal(5);
 
-console.log(new Tensor(tensor).toString())
+		const t = a1.t();
+		const tt = t.t();
+		expect(tt.toString()).to.equal(a1.toString());
+	});
 
-tensor = arange(5);
-console.log(tensor.toString());
-console.log(tensor.equal(arange(5)));
+	it('helper return types and single-element isNonzero', () => {
+		const floatFlag = isFloatingPoint(arange(5));
+		expect(typeof floatFlag).to.equal('boolean');
+
+		const single = arange(1);
+		const nonzeroFlag = isNonzero(single);
+		expect(typeof nonzeroFlag).to.equal('boolean');
+	});
+
+	it('empty tensor copy equality', () => {
+		const empty = new Tensor();
+		const emptyCopy = new Tensor(empty);
+		expect(emptyCopy.toString()).to.equal(empty.toString());
+	});
+});
